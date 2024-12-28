5ddcb8313bbe890b3b897379abd919f26af13599
nhtyy <n@push0.dev>
2024-12-28 10:18:30 -0800

    rc.3 release prep

--- src/edwards.rs
+++ src/edwards.rs
@@ -141,59 +141,58 @@ use backend::serial::scalar_mul;
 ))]
 use backend::vector::scalar_mul;
 
-cfg_if::cfg_if! {
-    if #[cfg(all(target_os = "zkvm", target_vendor = "succinct"))] {
-        use sp1_lib::{ed25519::Ed25519AffinePoint, utils::AffinePoint, syscall_ed_decompress};
-        use core::convert::TryInto;
+#[cfg(all(target_os = "zkvm", target_vendor = "succinct"))]
+mod conversions {
+use sp1_lib::{ed25519::Ed25519AffinePoint, utils::AffinePoint, syscall_ed_decompress};
+    use core::convert::TryInto;
 
-        impl From<EdwardsPoint> for Ed25519AffinePoint {
-            fn from(value: EdwardsPoint) -> Self {
-                let mut limbs = [0u32; 16];
+    impl From<EdwardsPoint> for Ed25519AffinePoint {
+        fn from(value: EdwardsPoint) -> Self {
+            let mut limbs = [0u32; 16];
 
-                // Invert `Z` inside an unconstrained block to normalize the point.
-                sp1_lib::unconstrained! {
-                    let mut buf = value.Z.invert().to_bytes();
-                    sp1_lib::io::hint_slice(&buf);
-                }
-
-                // Check that the hint is canonical, and an inverse.
-                let z_inv = read_and_verify_canon().unwrap();
-                assert!(&z_inv * &value.Z == FieldElement::one(), "The inverse of z is not correct. This is a bug.");
-
-                // Multiply by `z_inv` to normalize the point.
-                let value_x = &value.X * &z_inv;
-                let value_y = &value.Y * &z_inv;
-
-                // Convert the x and y coordinates to little endian u32 limbs.
-                // Calling `to_bytes()` guarantees canonical form.
-                for (x_limb, x_bytes) in limbs[..8]
-                    .iter_mut()
-                    .zip(value_x.to_bytes().chunks_exact(4))
-                {
-                    *x_limb = u32::from_le_bytes(x_bytes.try_into().unwrap());
-                }
-                for (y_limb, y_bytes) in limbs[8..]
-                    .iter_mut()
-                    .zip(value_y.to_bytes().chunks_exact(4))
-                {
-                    *y_limb = u32::from_le_bytes(y_bytes.try_into().unwrap());
-                }
-
-                Self { 0: limbs }
+            // Invert `Z` inside an unconstrained block to normalize the point.
+            sp1_lib::unconstrained! {
+                let mut buf = value.Z.invert().to_bytes();
+                sp1_lib::io::hint_slice(&buf);
             }
-        }
 
-        impl From<Ed25519AffinePoint> for EdwardsPoint {
-            fn from(value: Ed25519AffinePoint) -> Self {
-                let le_bytes = value.to_le_bytes();
-                let x = FieldElement::from_bytes(&le_bytes[..32].try_into().unwrap());
-                let y = FieldElement::from_bytes(&le_bytes[32..].try_into().unwrap());
-                let t = &x * &y;
+            // Check that the hint is canonical, and an inverse.
+            let z_inv = read_and_verify_canon().unwrap();
+            assert!(&z_inv * &value.Z == FieldElement::one(), "The inverse of z is not correct. This is a bug.");
 
-                Self { X: x, Y: y, Z: FieldElement::one(), T: t }
+            // Multiply by `z_inv` to normalize the point.
+            let value_x = &value.X * &z_inv;
+            let value_y = &value.Y * &z_inv;
+
+            // Convert the x and y coordinates to little endian u32 limbs.
+            // Calling `to_bytes()` guarantees canonical form.
+            for (x_limb, x_bytes) in limbs[..8]
+                .iter_mut()
+                .zip(value_x.to_bytes().chunks_exact(4))
+            {
+                *x_limb = u32::from_le_bytes(x_bytes.try_into().unwrap());
             }
+            for (y_limb, y_bytes) in limbs[8..]
+                .iter_mut()
+                .zip(value_y.to_bytes().chunks_exact(4))
+            {
+                *y_limb = u32::from_le_bytes(y_bytes.try_into().unwrap());
+            }
+
+            Self { 0: limbs }
         }
-    } 
+    }
+
+    impl From<Ed25519AffinePoint> for EdwardsPoint {
+        fn from(value: Ed25519AffinePoint) -> Self {
+            let le_bytes = value.to_le_bytes();
+            let x = FieldElement::from_bytes(&le_bytes[..32].try_into().unwrap());
+            let y = FieldElement::from_bytes(&le_bytes[32..].try_into().unwrap());
+            let t = &x * &y;
+
+            Self { X: x, Y: y, Z: FieldElement::one(), T: t }
+        }
+    }
 }
 
 // ------------------------------------------------------------------------
